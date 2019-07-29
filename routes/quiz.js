973d40ff5e74433a1b55f401776feb99cfdb1506
Franciszek Pawlak <franciszekpawlakit@gmail.com>
2019-07-29 19:51:54 +0200

    quiz

--- routes/quiz.js
+++ routes/quiz.js
@@ -1,11 +1,39 @@
 const express = require('express');
 const router = express.Router();
+const QuizSchema = require('../models/quiz')
 
 /* GET home page. */
 router.get('/', (req, res) => {
-    res.render('quiz', {
-        title: 'Quiz'
-    });
+    const show = !req.session.vote
+
+    QuizSchema.find({}, (err, data) => {
+        let sum = 0;
+        data.forEach((item => {
+            sum += item.vote
+        }))
+
+        res.render('quiz', {
+            title: 'Quiz',
+            data,
+            show,
+            sum
+        });
+    })
+});
+
+router.post('/', (req, res) => {
+    const id = req.body.quiz
+
+    QuizSchema.findOne({
+        _id: id
+    }, (err, data) => {
+        data.vote = data.vote + 1;
+        data.save(err => {
+            req.session.vote = 1
+            res.redirect('/quiz')
+        })
+
+    })
 });
 
 module.exports = router;
