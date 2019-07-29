7c6d16a79b87a4b1681d8c91c31905aa4078a55c
Franciszek Pawlak <franciszekpawlakit@gmail.com>
2019-07-29 18:16:13 +0200

    admin login, admin news

--- routes/admin.js
+++ routes/admin.js
@@ -1,11 +1,63 @@
 const express = require('express');
+const News = require('../models/news')
+
 const router = express.Router();
+
+
+router.all('*', (req, res, next) => {
+    if (!req.session.admin) {
+        res.redirect('login')
+        return
+    }
+    next()
+})
+
 
 /* GET home page. */
 router.get('/', (req, res) => {
-    res.render('admin', {
-        title: 'Admin'
-    });
+    const data = News.find({}, (err, data) => {
+        console.log(data)
+
+        res.render('admin/index', {
+            title: 'Admin',
+            data
+        });
+    })
+
+
 });
+
+router.get('/news/add', (req, res) => {
+    res.render('admin/news-form', {
+        title: 'add article',
+        body: {},
+        errors: {}
+    });
+})
+
+router.post('/news/add', (req, res) => {
+    const body = req.body
+    const newsData = new News(body)
+    const errors = newsData.validateSync()
+    newsData.save((err) => {
+        if (err) {
+            res.render('admin/news-form', {
+                title: 'add article',
+                errors,
+                body
+            });
+            return
+        } else {
+            res.redirect('/admin')
+        }
+    })
+})
+
+router.get('/news/delete/:id', (req, res) => {
+    News.findByIdAndDelete(req.params.id, (err) => {
+        res.redirect('/admin')
+    })
+
+})
 
 module.exports = router;
