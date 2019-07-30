56c9b545877887842e25baf7a0f5d037c0ca15ee
Franciszek Pawlak <franciszekpawlakit@gmail.com>
2019-07-30 10:45:54 +0200

    config db

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    db: '',
+    db: 'mongodb+srv://FranciszekPawlak:rWYr0KP8QpeuJXOT@cluster0-2rnyi.mongodb.net/test?retryWrites=true&w=majority',
     keySession: ['YOURKEY'],
     maxAge: 24 * 60 * 60 * 1000
 }
